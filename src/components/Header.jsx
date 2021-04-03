6b758c77fdf784f7c48a0cf25c1ac96ba28d9741
iylinkim <kiylin94@naver.com>
2021-04-03 15:46:15 +0900

    styles Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import styles from "../style/header.module.css";
+import styles from "style/header.module.css";
+
 const Header = (props) => {
-  return <header>[{new Date(Date.now()).toLocaleDateString()}] 김예린</header>;
+  return (
+    <header className={styles.header}>
+      [{new Date(Date.now()).toLocaleDateString()}] 김예린
+    </header>
+  );
 };
 
 export default Header;
--- src/components/Router.js
+++ src/components/Router.js
@@ -3,18 +3,21 @@ import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Detail from "../routes/Detail";
 import Home from "../routes/Home";
 import Header from "./Header";
+import styles from "style/router.module.css"
 
 const AppRouter = ({ api }) => {
   return (
     <Router>
       <Header />
       <Switch>
-        <Route exact path="/">
-          <Home api={api} />
-        </Route>
-        <Route exact path="/:id" >
+        <div className={styles.wrap}>
+          <Route exact path="/">
+            <Home api={api} />
+          </Route>
+          <Route exact path="/:id">
             <Detail api={api} />
-        </Route>
+          </Route>
+        </div>
       </Switch>
     </Router>
   );
